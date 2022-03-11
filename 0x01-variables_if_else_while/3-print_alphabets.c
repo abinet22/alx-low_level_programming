77164c024cdcc8c1127955826a5cc6a6577a2285
Abinet22 <abinet22@gmail.com>
2022-03-11 19:15:09 +0300

    print-alphabelts

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char l;
+char m;
+
+	l = 'a';
+    m = 'A';
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+    while (m <= 'Z')
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+return (0);
+}
