03f03fccc7488065fc42db4fd88480201eac875f
Abinet22 <abinet22@gmail.com>
2022-03-11 18:59:31 +0300

    lastlogic

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+   char ch;
+
+   for(ch = 'a' ; ch <= 'z' ; ch++) {
+      putchar(ch);
+   }
+
+	return (0);
+}
