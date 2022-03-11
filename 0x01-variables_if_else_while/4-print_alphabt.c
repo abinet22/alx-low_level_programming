624cc24c77e764f4ba446e1734afb3e192cfddc1
Abinet22 <abinet22@gmail.com>
2022-03-11 19:39:39 +0300

    print-alphabelts

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char l;
+char m;
+
+l = 'a';
+while (l <= 'z')
+{
+if(l == 'q' || l == 'e')
+{
+
+}
+else
+{
+putchar(l);
+}
+l++;
+}
+putchar('\n');
+return (0);
+}
