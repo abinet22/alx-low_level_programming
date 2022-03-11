a2a88c77d90306cb809b15cad8f45ad86c7a2b31
Abinet22 <abinet22@gmail.com>
2022-03-11 18:41:57 +0300

    lastlogic

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,24 +2,32 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
+	int r;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 5)
+
+	r = n % 10;
+	if (r > 5)
 	{
-		printf("%i and is greater than 5\n", n);
+		printf("Last digit of %i is %i and is greater than 5\n", n, r);
 	}
-	else if (n == 0)
+	else if (r == 0)
 	{
-		printf("%i and is 0\n", n);
+		printf("Last digit of %i is %i and is 0\n", n, r);
 	}
-	else if (n < 0 && n !=0)
+	else
 	{
-		printf("%i and is less than 6 and not 0\n", n);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, r);
 	}
+
 	return (0);
 }
