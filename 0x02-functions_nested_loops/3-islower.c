000a47539fb2a93afa85a691dbc599eb6af2d8bd
Abinet22 <abinet22@gmail.com>
2022-03-16 01:14:56 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _islower('H');
+    	for (i = 'a'; i <= 'z'; i++)
+        {
+            if (r == i)
+            {
+return (1);
+            }
+        }
+	
+    
+    return (0);
+}
