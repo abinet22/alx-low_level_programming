0e91153a48080842a29b5f562aaa50812aef5cea
Abinet22 <abinet22@gmail.com>
2022-03-16 01:50:23 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is lowercase.
+ * @n: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int _abs(int n)
+{
+	int m = abs(n);
+    _putchar(m);
+    return(0);
+}
