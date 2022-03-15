34b3bed13e76799fd3d3063d4adccefe8aca53d3
Abinet22 <abinet22@gmail.com>
2022-03-16 01:39:50 +0300

    alphabet

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,23 +9,23 @@
 int print_sign(int n)
 {
 if (n > 0)
+{
 _putchar('+');
-_putchar(',');
-_putchar(' ');
 _putchar('\n');
 return(1);
+}
 else if (n = 0)
+{
 _putchar('0');
-_putchar(',');
-_putchar(' ');
 _putchar('\n');
 return(0);
+}
 else if (n < 0)
+{
 _putchar('-');
-_putchar(',');
-_putchar(' ');
 _putchar('\n');
 return (-1);
+}
 
 
 }
