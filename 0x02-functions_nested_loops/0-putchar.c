0db717b5f7094948b1d36333e1528bec5e40fd93
Abinet22 <abinet22@gmail.com>
2022-03-16 00:26:37 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,10 +9,9 @@
 int main(void)
 {
     int i = 0;
-	char c;
-	char s[] = "_putchar\n";
-
-	while (i <= 9)
+    char c;
+    char s[] = "_putchar\n";
+    while (i < 9)
 	{
 		c = s[i];
 		_putchar(c);
