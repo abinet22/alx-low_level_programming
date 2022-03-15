2179fe4d33b978c4960415a34be69bab4cfa9717
Abinet22 <abinet22@gmail.com>
2022-03-16 00:38:00 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /* betty style doc for function main goes there */
 /**
  * main: Prints _putchar with _putchar
- * description: Prints _putchar with _putchar
+ * Description: Prints _putchar with _putchar
  * Return: Always 0 (Success)
  */
 
