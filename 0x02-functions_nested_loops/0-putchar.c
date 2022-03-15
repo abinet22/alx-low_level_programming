127369dced9744bbad47e54159375a7bef8b5d2f
Abinet22 <abinet22@gmail.com>
2022-03-16 00:15:54 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
 #include <main.h>
 
 /* betty style doc for function main goes there */
 /**
- * main - Entry point
+ * Description: Prints _putchar with _putchar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("_putchar\n");
+    char c= "_putchar\n";
+	_putchar(c);
 
 	return (0);
 }
