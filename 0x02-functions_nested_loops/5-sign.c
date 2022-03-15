5a3c26c3101d0817e8114295f5d71cb4931b1bf4
Abinet22 <abinet22@gmail.com>
2022-03-16 01:44:31 +0300

    alphabet

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,28 @@
 #include "main.h"
 
 /**
- * print_sign - Checks if a character is alpha.
- * @n: The character to be checked.
+ * print_sign - Prints the sign of a number.
+ * @n: The number of which the sign will be printed.
  *
- * Return: 1 if character is lowercase, 0 otherwise.
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero.
  */
 int print_sign(int n)
 {
-if (n > 0)
-{
-_putchar('+');
-_putchar('\n');
-return(1);
-}
-if (n == 0)
-{
-_putchar('0');
-_putchar('\n');
-return(0);
-}
-if (n < 0)
-{
-_putchar('-');
-_putchar('\n');
-return (-1);
-}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
