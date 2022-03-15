a3aff972e7f71d632e2bedbd9f5036882ca2cc2e
Abinet22 <abinet22@gmail.com>
2022-03-16 00:56:27 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+    while(int i = 0;i<=10)
+    for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+    
+    return (0);
+}
