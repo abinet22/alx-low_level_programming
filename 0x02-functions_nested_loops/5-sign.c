6363c7a13db51061f60718b5eae12b97b66f6e75
Abinet22 <abinet22@gmail.com>
2022-03-16 01:37:38 +0300

    alphabet

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,19 +13,19 @@ _putchar('+');
 _putchar(',');
 _putchar(' ');
 _putchar('\n');
-return(1)
+return(1);
 else if (n = 0)
 _putchar('0');
 _putchar(',');
 _putchar(' ');
 _putchar('\n');
-return(0)
+return(0);
 else if (n < 0)
 _putchar('-');
 _putchar(',');
 _putchar(' ');
 _putchar('\n');
 return (-1);
-else
+
 
 }
