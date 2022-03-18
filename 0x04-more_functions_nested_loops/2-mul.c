591c97cbd915504ea91dab4ce2f384e602f77d24
Abinet22 <abinet22@gmail.com>
2022-03-18 13:02:55 +0300

    isdigit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks  for a digit (0 through 9).
+ * @a: The character to be checked.
+ * @b: The character to be checked.
+ *
+ * Return: 1 if is a digit, 0 otherwise.
+ */
+int mul(int a,int b)
+{
+  int result =  a*b;
+return (0);
+}
