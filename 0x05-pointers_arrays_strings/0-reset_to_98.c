b9531954a04a1478b40d672c6bdd2dfe9cb3b9d3
Abinet Tefera <abinet22@gmail.com>
2022-03-21 23:03:01 +0300

    first change

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,11 +4,15 @@
  * reset_to_98 -  Takes a pointer to an int as parameter and updates the value it points to to 98.
  * @n: The parameter to be checked.
  *
- * Return: 0.
+ * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-int c = 98;
-*n=&c;
-return (0);
+   int n;
+
+n = 402;
+_putchar(n);
+*n = 98;
+_putchar(n);
+return(0);
 }
