62fd46c24d806d3052624e5b3794481f5d1bfcd6
Abinet Tefera <abinet22@gmail.com>
2022-04-02 02:30:57 +0300

    static libraries

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * main - Prints _putchar with _putchar
+ * Description: Prints _putchar with _putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i = 0;
+char c;
+char s[] = "_putchar\n";
+while (i < 9)
+{
+c = s[i];
+_putchar(c);
+i++;
+}
+
+return (0);
+}
